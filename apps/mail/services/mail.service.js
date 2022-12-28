630061144aa728b18decf0950d15da38ca43b487
Gilad Dagan <giladagan53@gmail.com>
2022-12-28 12:45:28 +0200

    test

--- apps/mail/services/mail.service.js
+++ apps/mail/services/mail.service.js
@@ -1,1 +1,41 @@
-console.log('Hi')
+import { utilService } from "../../../services/util.service.js"
+import { storageService } from "../../../services/storage.service.js"
+import { storageService } from "../../../services/async-storage.service.js"
+
+const MAIL_KEY = 'mailDB'
+
+
+export const mailService = {
+    query
+}
+
+_createMails()
+
+
+function query() {
+    return storageService.query(MAIL_KEY)
+
+}
+
+
+function _createMails() {
+    let mails = storageService.loadFromStorage(MAIL_KEY)
+    if (!mails || !mails.length) {
+        mails = []
+        mails.push(_createMail('Meeting', 'Hey! waiting from response', new Date().toLocaleDateString(), 'momo@gmail.com'))
+        mails.push(_createMail('Party', 'Are you coming to the party tommorrow?', new Date().toLocaleDateString(), 'momo@gmail.com'))
+        storageService.saveToStorage(MAIL_KEY, mails)
+    }
+}
+
+
+function _createMail(subject, body, sentAt, to) {
+    return {
+        id: utilService.makeId(),
+        subject,
+        body,
+        sentAt,
+        isRead: false,
+        to,
+    }
+}
