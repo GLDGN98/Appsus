939c1c9990d0879dfabe0a8c1bd15dff78b8d4a1
GalZohar4021 <118897719+GalZohar4021@users.noreply.github.com>
2022-12-29 13:25:06 +0200

    root-cmp added route path note/edit

--- root-cmp.jsx
+++ root-cmp.jsx
@@ -4,10 +4,12 @@ const Router = ReactRouterDOM.HashRouter
 import { AppHeader } from "./cmps/app-header.jsx"
 import { About } from "./views/about.jsx"
 import { Home } from "./views/home.jsx"
+
 import { MailIndex } from "./apps/mail/views/mail-index.jsx"
-import { NoteIndex } from "./apps/note/views/note-index.jsx"
 import { MailDetails } from "./apps/mail/views/mail-details.jsx"
 
+import { NoteIndex } from "./apps/note/views/note-index.jsx"
+import { NoteEdit } from "./apps/note/cmps/note-edit.jsx"
 
 
 export function App() {
