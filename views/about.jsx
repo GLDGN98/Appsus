8face86191b1974b58b046fb05053cb5d9603b99
GalZohar4021 <118897719+GalZohar4021@users.noreply.github.com>
2022-12-29 21:59:01 +0200

    Card background - about page

--- views/about.jsx
+++ views/about.jsx
@@ -8,26 +8,22 @@ export function About() {
             title: 'Fullstack Developer',
             txt: 'I\'m a 27 from Oranit, Israel. I am a junior developr who learn Fullstack development at Coding Academy.',
             img: '../assets/img/dev/gal.jpg',
-            contact: [
-                {
-                    facebook: 'https://www.facebook.com/profile.php?id=100077018276598',
-                    github: 'https://github.com/GalZohar4021',
-                    linkdin: 'https://www.linkedin.com/in/gal-zohar-aa0101184'
-                }
-            ]
+            contact: {
+                facebook: 'https://www.facebook.com/profile.php?id=100077018276598',
+                github: 'https://github.com/GalZohar4021',
+                linkdin: 'https://www.linkedin.com/in/gal-zohar-aa0101184'
+            }
         },
         {
             name: 'Gilad Dagan',
             title: 'Fullstack Developer',
             txt: 'I\'m a 24 from Herzliya, Israel. I am a junior developr who learn Fullstack development at Coding Academy.',
             img: '../assets/img/dev/gilad.jpg',
-            contact: [
-                {
-                    facebook: '',
-                    github: '',
-                    linkdin: ''
-                }
-            ]
+            contact: {
+                facebook: 'https://www.facebook.com/gilad.dagan.9',
+                github: 'https://github.com/GLDGN98',
+                linkdin: ''
+            }
         }
     ]
     return <section className="about">
