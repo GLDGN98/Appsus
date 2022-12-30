6ad807f0b47eff8f9b67d96b8c8700e07eacf8c7
Gilad Dagan <giladagan53@gmail.com>
2022-12-30 13:19:46 +0200

    added long-txt cmp

--- apps/mail/cmps/mail-compose.jsx
+++ apps/mail/cmps/mail-compose.jsx
@@ -1,11 +1,34 @@
-const { useState } = React
+import { showSuccessMsg } from "../../../services/event-bus.service.js"
+import { mailService } from "../services/mail.service.js"
+
+const { useState, useRef, useEffect } = React
 const { useNavigate } = ReactRouterDOM
 
 
 
-export function MailCompose({ sendMail, showNewMessage, setShowNewMessage }) {
-    const [newMessage, setNewMessage] = useState({ to: '', subject: '', body: '', name: 'Mahatma Appsus', from: "user@appsus.com", sentAt: new Date(), removedAt: null })
+export function MailCompose({ sendMail, showNewMessage, setShowNewMessage, isDrafted }) {
+    const [newMessage, setNewMessage] = useState({ to: '', subject: '', body: '', name: 'Mahatma Appsus', from: "user@appsus.com", sentAt: new Date(), removedAt: null, isRead: true, isSent: false })
     const navigate = useNavigate()
+    const formRef = useRef(null)
+    let mailDraftInterval = useRef(null)
+
+
+
+    // useEffect(() => {
+    //     console.log(isDrafted)
+
+    //     if (isDrafted === true) {
+    //         mailDraftInterval.current = setInterval(() => {
+    //             saveDraftedMails(newMessage)
+    //         }, 5000)
+    //     }
+
+    //     if (isDrafted === false) {
+    //         clearInterval(mailDraftInterval.current)
+    //         setNewMessage.sentAt = new Date()
+    //     }
+
+    // }, [isDrafted])
 
     function handleChange({ target }) {
         let { value, name: field } = target
@@ -13,17 +36,33 @@ export function MailCompose({ sendMail, showNewMessage, setShowNewMessage }) {
     }
 
 
-
     function onSendMail(ev) {
         ev.preventDefault()
         sendMail(newMessage)
         setShowNewMessage(false)
+        navigate('/mail/sent-mail')
+        formRef.current.reset();
+        showSuccessMsg('Mail sent successfully!')
     }
+
+    //     Add a draft folder – email that is being composed is auto saved every 5 
+    // seconds and can be viewed in the draft folder until sent
+
+    // function saveDraftedMails(mail) {
+    //     console.log('ho')
+
+    // }
+
 
     return (
         <div className="mail-compose">
-            <form className={showNewMessage ? 'new-mail active' : "new-mail"} onSubmit={onSendMail}>
-                <h3>New Message</h3>
+            <form ref={formRef} className={showNewMessage ? 'new-mail active' : "new-mail"} onSubmit={onSendMail}>
+                <div className="new-message-header-bg">
+                    <div className="new-message-header">
+                        <h3>New Message</h3>
+                        <i class="fa-solid fa-trash-can"></i>
+                    </div>
+                </div>
                 <div>
                     <input
                         id="to"
--- apps/mail/cmps/mail-preview.jsx
+++ apps/mail/cmps/mail-preview.jsx
@@ -1,5 +1,7 @@
 import { mailService } from "../services/mail.service.js"
 
+import { LongTxt } from "./long-txt.jsx"
+
 const { useState, useEffect, Fragment } = React
 const { Link } = ReactRouterDOM
 
@@ -34,18 +36,19 @@ export function MailPreview({ mail, handleDelete }) {
             }}>
                 <td onClick={handleStar}>{mail.starred ? <i style={{ color: 'gold' }} className="fa-sharp fa-solid fa-star"></i> : <i className="fa-regular fa-star"></i>}</td>
                 <td>{mail.name}</td>
-                <td>{mail.body}</td>
+                {/* <td>{mail.body}</td> */}
+                <td><LongTxt txt={mail.body} length={120} /></td>
                 <td className="mail-sent-at">
                     <span className="sent-at">{new Date(mail.sentAt).toLocaleDateString()}</span>
                     <div className="hover-icons">
                         <Link to={`/mail/inbox/${mail.id}`}><span title="Expand" className="expand-icon"><i className="fa-solid fa-expand"></i></span></Link>
-                        <span title="Delete" className="trash-icon"><i onClick={(ev) => onHandleDelete(ev, mail.id)} className="fa-sharp fa-solid fa-trash"></i></span>
-                        <span onClick={onRead} title="Read" className="unread-icon"><i className="fa-regular fa-envelope"></i></span>
+                        <span title="Delete" className="trash-icon"><i onClick={(ev) => onHandleDelete(ev, mail.id)} className="fa-solid fa-trash-can"></i></span>
+                        <span onClick={onRead} className="unread-icon">{mail.isRead ? <i title="Unread" class="fa-solid fa-envelope-open"></i> : <i title="Read" class="fa-solid fa-envelope"></i>}</span>
                     </div>
                 </td>
             </tr>
             <tr className="second-tr" hidden={!isExpanded}>
-                <td colSpan="3">
+                <td style={{ borderBottom: '1px solid #d1dae0' }} colSpan="3">
                     <h2>{mail.name}</h2>
                     <h3>&lt;{mail.from}&gt;</h3>
                     <h3>{mail.subject}</h3>
@@ -53,6 +56,6 @@ export function MailPreview({ mail, handleDelete }) {
                     <Link to={`/mail/inbox/${mail.id}`}><i className="fa-sharp fa-solid fa-expand"></i></Link>
                 </td>
             </tr>
-        </Fragment>
+        </Fragment >
     )
 }
