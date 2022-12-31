e56da4c11c5b4c2a054acdb10a11f863f9cacf83
Gilad Dagan <giladagan53@gmail.com>
2022-12-31 19:48:13 +0200

    sync

--- apps/mail/cmps/mail-filter.jsx
+++ apps/mail/cmps/mail-filter.jsx
@@ -28,16 +28,8 @@ export function MailFilter({ onSetFilter, sortBy, setShowNav, showNav }) {
         setShowNav((prev) => !prev)
     }
 
-    function toggleMenu() {
-        document.body.classList.toggle('menu-open-mail')
-    }
-
-
     return (
         <div className="mail-filter">
-            <div onClick={toggleMenu} className="menu-mobile">
-                <i className="fa-solid fa-bars"></i>
-            </div>
             <div className="search-wrapper">
                 <i className="fa-sharp fa-solid fa-magnifying-glass"></i>
                 <input
